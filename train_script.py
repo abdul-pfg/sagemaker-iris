68a0ff908a24166deb39999c8dbf3e8f5f090206
abdul-pfg <66666645+abdul-pfg@users.noreply.github.com>
2020-06-22 20:26:52 +0530

    Update train_script.py

--- train_script.py
+++ train_script.py
@@ -3,11 +3,11 @@ import boto3
 import os
 
 
-#USER VARIABLES
-#role = os.environ['role']
-#train_script = os.environ['train_script']
-#source = os.environ['source']
-#train_data = os.environ['train_data']
+#ENV VARIABLES
+ROLE = os.environ.get('ROLE')
+TRAIN_SCRIPT = os.environ.get('TRAIN_SCRIPT')
+SOURCE = os.environ.get('SOURCE')
+TRAIN_DATA = os.environ.get('TRAIN_DATA')
 
 
 def train():
@@ -15,29 +15,18 @@ def train():
     try:
 
         #Create a sagemaker.sklearn.SKLearn Estimator
-
-        #print('start')
-        #print('create SKlearn obj')
-        
-        proxy = os.environ.get('PROXY')
-        #os.environ['http_proxy'] = proxy 
-        #os.environ['HTTP_PROXY'] = proxy
-        #os.environ['https_proxy'] = proxy
-        #os.environ['HTTPS_PROXY'] = proxy
-        
-        aws_sklearn = SKLearn(entry_point='sklearn-iris-main.py',
-                            source_dir='s3://aws-sagemaker-iris-us-east-2-304472691870/sklearn-iris-main.py.tar.gz',
+    
+        aws_sklearn = SKLearn(entry_point=TRAIN_SCRIPT,
+                            source_dir=SOURCE,
                             train_instance_type='ml.m4.xlarge',
-                            role='aws-sagemaker-role-s843971')
+                            role=ROLE)
                             
         #Call the fit method on SKlearn estimator which uses our python script to train the model
         
-        print('run fit method')
-       
-        aws_sklearn.fit({'train':'s3://aws-sagemaker-iris-us-east-2-304472691870/train'})
+        aws_sklearn.fit({'train':TRAIN_DATA})
 
         #Deploy the model created in previous step and create an endpoint
-        print('deploy')
+        
         aws_sklearn_predictor = aws_sklearn.deploy(instance_type='ml.m4.xlarge', 
                                                 initial_instance_count=1)
 
@@ -46,8 +35,10 @@ def train():
     else:
         return 'success'
 
+
 if __name__ =='__main__':
     train()
+
 
 
     
